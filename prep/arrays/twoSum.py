06db2333c1e6bbeb5890dbd85a43c289b7ba8b5a
myakovlev <m.yakovlev@usa.com>
2022-06-15 22:41:13 -0400

    HashMap Solution for Two Sum

--- prep/arrays/twoSum.py
+++ prep/arrays/twoSum.py
@@ -2,6 +2,8 @@
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
 '''
+
+# Solution 1
 def twoSum(nums, target):
     for i in range(len(nums)):
         for k in range(i+1, len(nums)):
@@ -9,4 +11,10 @@ def twoSum(nums, target):
                 return [i, k]
 
 
-
+# Solution 2
+def twoSum(nums, target):
+    hm = {}
+    for i in range(len(nums)):
+        if (target - nums[i]) in hm:
+            return [hm[target-nums[i]], i]
+        hm[nums[i]] = i
