44bd9d0a1b079f06d109d61b21e0c647aa440fd1
myakovlev <m.yakovlev@usa.com>
2022-07-20 22:02:12 -0400

    Invert Binary Tree

---
+++ prep/trees/invertTree.py
@@ -0,0 +1,25 @@
+'''
+Given the root of a binary tree, invert the tree, and return its root.
+'''
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: 'optional[TreeNode') -> 'optional[TreeNode]':
+        if not root:
+            return None
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
